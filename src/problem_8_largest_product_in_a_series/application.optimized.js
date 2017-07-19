8e9582afbc1fabe05028b4ba28e2b8f30fd34140
Alexander <abkolodiychuk@gmail.com>
2017-07-19 23:45:58 +0200

    Two solutions for problem 8. Ignoring generated documentation

---
+++ src/problem_8_largest_product_in_a_series/application.optimized.js
@@ -0,0 +1,69 @@
+/**
+ * @description
+ * <p>
+ *   Optimized way to calculate max product of serial digits. Instead of recounting
+ *   each serial digits product, multiplications are reduced to only recalculating
+ *   changed part of a product.
+ * </p>
+ *
+ * <p>
+ *   For example when comparing products of [1, 2, 3, 4] and [2, 3, 4, 5], instead of
+ *   comparing 1 * 2 * 3 * 4 and 2 * 3 * 4 * 5, in this implementation we calculate
+ *   product of 1 * 2 * 3 * 4 and then multiply it by 5 (new number from the next set)
+ *   and divide it by 1 (the first number of the previous set). This helps to reduce
+ *   a number of multiplications by {digitsCount} - 2 on each set of digits.
+ * </p>
+ *
+ * @param {String} number - a source number from which serial digits product is calculated from.
+ * @param {Number} digitsCount - count of serial digits in number to calculate product from.
+ * @returns {number} largest product of {digitsCount} serial digits from {number}.
+ *
+ * @see specific behavior for zeros in sets
+ */
+const largestSerialDigitsProduct = (number, digitsCount) => {
+  let maxProduct = 0;
+  let product = productOf(number.substring(0, digitsCount));
+  for (let i = 0; i < number.length - digitsCount; i++) {
+    const prevFirst = +number[i];
+    const nextLast = +number[i + digitsCount];
+
+    if (nextLast) {
+      product *= nextLast;
+    } else {
+      product = 0;
+      continue;
+    }
+
+    if (prevFirst) {
+      product /= prevFirst;
+    } else {
+      product = productOf(number.substring(i + 1, i + 1 + digitsCount));
+    }
+
+    if (product > maxProduct) {
+      maxProduct = product;
+    }
+  }
+
+  return maxProduct;
+};
+
+const productOf = (chunk) => {
+  if (chunk.indexOf('0') === -1) {
+    return numberArrFrom(chunk).reduce((product, next) => product * next);
+  } else {
+    return 0;
+  }
+};
+
+const numberArrFrom = (chunk) => {
+  return chunk
+    .split('')
+    .map(c => +c);
+};
+
+module.exports = {
+  largestSerialDigitsProduct,
+  productOf,
+  numberArrFrom
+};
--- src/problem_8_largest_product_in_a_series/application.spec.js
+++ src/problem_8_largest_product_in_a_series/application.spec.js
@@ -1,11 +1,15 @@
 'use strict';
 
-const application = require('./application');
+/**
+ * Switch between application.optimized and application.simple
+ * to compare performance
+ */
+const application = require('./application.optimized');
 const expect = require('chai').expect;
 
 describe('problems 8: finding largest product in a series', () => {
 
-  const thousandDigit =
+  const thousandDigitNumber =
     '73167176531330624919225119674426574742355349194934' +
     '96983520312774506326239578318016984801869478851843' +
     '85861560789112949495459501737958331952853208805511' +
@@ -27,16 +31,70 @@ describe('problems 8: finding largest product in a series', () => {
     '05886116467109405077541002256983155200055935729725' +
     '71636269561882670428252483600823257530420752963450';
 
-  it('gets 4 adjacent digits largest product', () => {
-    const largestProduct = application.largestAdjacentNumbersProduct(thousandDigit, 4);
-    console.log(`Largest product of 4 adjacent numbers in given 100-digit number is: ${largestProduct}`);
+  it('gets 2 serial digits largest product', () => {
+    const largestProduct = application.largestSerialDigitsProduct(thousandDigitNumber, 2);
+    console.log(`Largest product of 2 serial digits in given 1000-digit number is: ${largestProduct}`);
+    expect(largestProduct).to.equal(81);
+  });
+
+  it('gets 3 serial digits largest product', () => {
+    const largestProduct = application.largestSerialDigitsProduct(thousandDigitNumber, 3);
+    console.log(`Largest product of 3 serial digits in given 1000-digit number is: ${largestProduct}`);
+    expect(largestProduct).to.equal(648);
+  });
+
+  it('gets 4 serial digits largest product', () => {
+    const largestProduct = application.largestSerialDigitsProduct(thousandDigitNumber, 4);
+    console.log(`Largest product of 4 serial digits in given 1000-digit number is: ${largestProduct}`);
     expect(largestProduct).to.equal(5832);
   });
 
-  it('gets 13 adjacent digits largest product', () => {
-    const largestProduct = application.largestAdjacentNumbersProduct(thousandDigit, 13);
-    console.log(`Largest product of 13 adjacent numbers in given 100-digit number is: ${largestProduct}`);
-    expect(largestProduct).to.equal(23514624000); // 15ms
+  it('gets 5 serial digits largest product', () => {
+    const largestProduct = application.largestSerialDigitsProduct(thousandDigitNumber, 5);
+    console.log(`Largest product of 5 serial digits in given 1000-digit number is: ${largestProduct}`);
+    expect(largestProduct).to.equal(40824);
+  });
+
+  it('gets 6 serial digits largest product', () => {
+    const largestProduct = application.largestSerialDigitsProduct(thousandDigitNumber, 6);
+    console.log(`Largest product of 6 serial digits in given 1000-digit number is: ${largestProduct}`);
+    expect(largestProduct).to.equal(285768);
+  });
+
+  it('gets 7 serial digits largest product', () => {
+    const largestProduct = application.largestSerialDigitsProduct(thousandDigitNumber, 7);
+    console.log(`Largest product of 7 serial digits in given 1000-digit number is: ${largestProduct}`);
+    expect(largestProduct).to.equal(2571912);
+  });
+
+  it('gets 13 serial digits largest product', () => {
+    const largestProduct = application.largestSerialDigitsProduct(thousandDigitNumber, 13);
+    console.log(`Largest product of 13 serial digits in given 1000-digit number is: ${largestProduct}`);
+    expect(largestProduct).to.equal(23514624000);
+  });
+
+  it('gets 18 serial digits largest product', () => {
+    const largestProduct = application.largestSerialDigitsProduct(thousandDigitNumber, 18);
+    console.log(`Largest product of 18 serial digits in given 1000-digit number is: ${largestProduct}`);
+    expect(largestProduct).to.equal(25406375500800);
+  });
+
+  it('gets 25 serial digits largest product', () => {
+    const largestProduct = application.largestSerialDigitsProduct(thousandDigitNumber, 25);
+    console.log(`Largest product of 25 serial digits in given 1000-digit number is: ${largestProduct}`);
+    expect(largestProduct).to.equal(94810963968000000);
+  });
+
+  it('gets 30 serial digits largest product', () => {
+    const largestProduct = application.largestSerialDigitsProduct(thousandDigitNumber, 30);
+    console.log(`Largest product of 30 serial digits in given 1000-digit number is: ${largestProduct}`);
+    expect(largestProduct).to.equal(374476218826752000000);
+  });
+
+  it('gets 32 serial digits largest product', () => {
+    const largestProduct = application.largestSerialDigitsProduct(thousandDigitNumber, 32);
+    console.log(`Largest product of 32 serial digits in given 1000-digit number is: ${largestProduct}`);
+    expect(largestProduct).to.equal(2.097066825429811e+22);
   });
 
   it(`counts weight of number chunk by it's sum`, () => {
