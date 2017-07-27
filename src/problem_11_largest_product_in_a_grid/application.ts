2a4e18b6e66f6e3d4825a948dfce7e8b17949d81
Alexander <abkolodiychuk@gmail.com>
2017-07-27 22:51:28 +0200

    Problem 11: Preparing calculation lines

---
+++ src/problem_11_largest_product_in_a_grid/application.ts
@@ -0,0 +1,44 @@
+import * as _ from 'lodash';
+
+export class Grid extends Array<Array<number>> {
+
+  static parse(rawGrid: string): Grid {
+    return new Grid(...rawGrid
+      .split('\n')
+      .map(line => line
+        .split(' ').filter(c => c).map(c => +c)
+      )
+    );
+  }
+
+  columns() {
+    return new Grid(...Object.keys(this)
+      .map(k => +k)
+      .map(key => this
+        .map(row => row[key])
+      )
+    );
+  }
+
+  rows() {
+    return this;
+  }
+
+  leftDiagonals() {
+    const halfTriangle = this.leftTriangle();
+    const reversed = new Grid(...this.slice().reverse().map(line => line.slice().reverse()));
+    const rightTriangle = reversed.leftTriangle().slice(0, -1).reverse().map(line => line.slice().reverse());
+    return halfTriangle.concat(rightTriangle);
+  }
+
+  rightDiagonals() {
+    const reversedRows = new Grid(...this.map(line => line.reverse()));
+    return reversedRows.leftDiagonals();
+  }
+
+  private leftTriangle() {
+    return _.range(0, this.length)
+      .map(i => _.range(0, i + 1).map(j => this[j][i - j]))
+      .reduce((acc: number[][], arr) => acc.concat([arr]), []);
+  }
+}
