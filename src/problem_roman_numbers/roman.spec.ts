f45df17280da96f66635da7f90a8327b846f3c83
Alexander <alexander@pioneerlabs.io>
2022-11-04 19:09:36 +0100

    Roman / Decimal number converter

---
+++ src/problem_roman_numbers/roman.spec.ts
@@ -0,0 +1,92 @@
+import { expect } from 'chai';
+import { fromRoman, getSignCombos, subtractableSigns, fromDecimal } from './roman';
+
+describe(`roman / decimal converter`, () => {
+
+  describe(`convert roman to decimal`, () => {
+
+    it(`checks the subtractable signs`, () => {
+      expect(subtractableSigns).to.deep.equal([ 'I', 'X', 'C', 'M' ]);
+    });
+
+    it(`gets the subtractable combos`, () => {
+      expect(getSignCombos('I')).to.deep.equal([
+        { 'IV': 4 },
+        { 'IX': 9 },
+      ]);
+
+      expect(getSignCombos('X')).to.deep.equal([
+        { 'XL': 40 },
+        { 'XC': 90 },
+      ]);
+    });
+
+    it(`converts a simple roman number`, () => {
+      expect(fromRoman('I')).to.equal(1);
+      expect(fromRoman('V')).to.equal(5);
+      expect(fromRoman('X')).to.equal(10);
+    });
+
+    it(`converts combo roman numbers`, () => {
+      expect(fromRoman('IV')).to.equal(4);
+      expect(fromRoman('IX')).to.equal(9);
+      expect(fromRoman('XL')).to.equal(40);
+      expect(fromRoman('XC')).to.equal(90);
+      expect(fromRoman('CD')).to.equal(400);
+      expect(fromRoman('CM')).to.equal(900);
+    });
+
+    it(`converts complex roman numbers`, () => {
+      expect(fromRoman('XIV')).to.equal(14);
+      expect(fromRoman('LIX')).to.equal(59);
+      expect(fromRoman('XXXIII')).to.equal(33);
+      expect(fromRoman('XCIX')).to.equal(99);
+      expect(fromRoman('MCDXLIV')).to.equal(1444);
+      expect(fromRoman('MMCMXCIX')).to.equal(2999);
+    });
+
+  });
+
+  describe(`convert decimal to roman`, () => {
+
+    it(`converts perfect match decimals to roman`, () => {
+      expect(fromDecimal(1)).to.eq('I');
+      expect(fromDecimal(5)).to.eq('V');
+      expect(fromDecimal(10)).to.eq('X');
+      expect(fromDecimal(50)).to.eq('L');
+      expect(fromDecimal(100)).to.eq('C');
+      expect(fromDecimal(500)).to.eq('D');
+      expect(fromDecimal(1000)).to.eq('M');
+    });
+
+    it(`converts perfect match decimals to combo roman`, () => {
+      expect(fromDecimal(4)).to.eq('IV');
+      expect(fromDecimal(9)).to.eq('IX');
+      expect(fromDecimal(40)).to.eq('XL');
+      expect(fromDecimal(90)).to.eq('XC');
+      expect(fromDecimal(400)).to.eq('CD');
+      expect(fromDecimal(900)).to.eq('CM');
+    });
+
+    it(`converts decimals to combined roman`, () => {
+      expect(fromDecimal(14)).to.eq('XIV');
+      expect(fromDecimal(44)).to.eq('XLIV');
+      expect(fromDecimal(99)).to.eq('XCIX');
+    });
+
+    it(`converts decimals to complex roman`, () => {
+      expect(fromDecimal(3)).to.eq('III');
+      expect(fromDecimal(7)).to.eq('VII');
+      expect(fromDecimal(8)).to.eq('VIII');
+      expect(fromDecimal(13)).to.eq('XIII');
+    });
+
+    it(`converts decimals to complex roman`, () => {
+      expect(fromDecimal(3449)).to.eq('MMMCDXLIX');
+      expect(fromDecimal(1996)).to.eq('MCMXCVI');
+      expect(fromDecimal(1848)).to.eq('MDCCCXLVIII');
+    });
+
+  });
+
+});
---
+++ src/problem_roman_numbers/roman.ts
@@ -0,0 +1,133 @@
+import * as _ from 'lodash';
+
+const romanSigns = {
+  'I': 1,
+  'V': 5,
+  'X': 10,
+  'L': 50,
+  'C': 100,
+  'D': 500,
+  'M': 1000,
+};
+
+export const decimalToRomanSigns: { [decimal: string]: RomanSign } = Object.keys(romanSigns)
+  .reduce((acc, next) => ({ ...acc, [romanSigns[next as RomanSign]]: next, }), {});
+export const decimalToRomanLevels = Object.keys(decimalToRomanSigns).map(key => +key);
+
+type RomanSign = keyof typeof romanSigns;
+type RomanToDecimalMap = { [roman: string]: number };
+
+const orderedRomanSigns = Object.keys(romanSigns);
+const romanSignsIndexes: RomanToDecimalMap = orderedRomanSigns.reduce((acc, next, index) => ({
+  ...acc,
+  [next]: index,
+}), {});
+
+export const subtractableSigns = orderedRomanSigns.filter((sign, index) => index % 2 === 0) as RomanSign[];
+
+export const getSignCombos = (subtractableSign: RomanSign) => {
+  if (!_.includes(subtractableSigns, subtractableSign)) {
+    throw new Error(`'${subtractableSign}' is not a subtractable roman sign`);
+  }
+  const index = romanSignsIndexes[subtractableSign];
+
+  const toBeSubtractedFromSigns = [
+    orderedRomanSigns[index + 1] as RomanSign,
+    orderedRomanSigns[index + 2] as RomanSign
+  ];
+
+  return toBeSubtractedFromSigns.filter(sign => sign).map(sign => ({
+    [`${subtractableSign}${sign}`]: romanSigns[sign] - romanSigns[subtractableSign]
+  }));
+};
+
+export const romanToDecimalCombos = _.flatten(
+  subtractableSigns.map(getSignCombos)
+)
+  .reduce((acc: RomanToDecimalMap, next: RomanToDecimalMap) => ({ ...acc, ...next }), {});
+
+const fromRomanTail = (romanNumber: string, sum = 0): number => {
+  if (romanNumber.length === 0) {
+    return sum;
+  }
+  if (romanNumber.length === 1) {
+    return sum + romanSigns[romanNumber as RomanSign];
+  }
+  const firstTwo = romanNumber.substring(0, 2);
+  const matchedCombo = romanToDecimalCombos[firstTwo];
+  if (!_.isNil(matchedCombo)) {
+    return fromRomanTail(romanNumber.slice(2), sum + matchedCombo);
+  }
+
+  const firstOne = romanNumber[0];
+  const matchedSingleSign = romanSigns[firstOne as RomanSign];
+  if (!_.isNil(matchedSingleSign)) {
+    return fromRomanTail(romanNumber.slice(1), sum + matchedSingleSign);
+  }
+
+  throw new Error(`Didn't match '${romanNumber}' to a roman number`);
+};
+
+const decimalToRomanCombos: { [decimal: string]: RomanSign } = Object.keys(romanToDecimalCombos)
+  .reduce((acc, next) => ({ ...acc, [romanToDecimalCombos[next]]: next }), {});
+
+const fromDecimalTail = (decimalStr: string, accRomanNumber: string): string => {
+  if (+decimalStr === 0) {
+    return accRomanNumber;
+  }
+
+  const fullMatchCombo = decimalToRomanCombos[decimalStr];
+  if (fullMatchCombo) {
+    return `${fullMatchCombo}${accRomanNumber}`;
+  }
+
+  const fullMatchRoman = decimalToRomanSigns[decimalStr];
+  if (fullMatchRoman) {
+    return `${fullMatchRoman}${accRomanNumber}`;
+  }
+
+  const endDecimalComboMatch = Object.keys(decimalToRomanCombos).find(combo => decimalStr.endsWith(combo));
+  if (endDecimalComboMatch) {
+    const romanCombo = decimalToRomanCombos[endDecimalComboMatch];
+    const remainingDecimalStr = `${+decimalStr - +endDecimalComboMatch}`;
+    return fromDecimalTail(remainingDecimalStr, `${romanCombo}${accRomanNumber}`)
+  }
+
+  const endDecimalSingleMatch = Object.keys(decimalToRomanCombos).find(combo => decimalStr.endsWith(combo));
+  if (endDecimalSingleMatch) {
+    const romanSign = decimalToRomanCombos[endDecimalSingleMatch];
+    const remainingDecimalStr = `${+decimalStr - +endDecimalSingleMatch}`;
+    return fromDecimalTail(remainingDecimalStr, `${romanSign}${accRomanNumber}`)
+  }
+
+  const firstZeroIndex = decimalStr.indexOf(`0`);
+  const complexDecimalToConvert = decimalStr.endsWith(`0`) && firstZeroIndex > 0 ? decimalStr.slice(firstZeroIndex - 1) : decimalStr.slice(-1);
+  const complexValue = +complexDecimalToConvert;
+
+  let complexValueLeft = complexValue;
+  let accComplexRoman = '';
+
+  while (complexValueLeft > 0) {
+    const firstHigherLevelDecimalIndex = decimalToRomanLevels.findIndex(decimal => +decimal > complexValueLeft);
+    const lastDecimalLessThanValue = firstHigherLevelDecimalIndex > 0 ? decimalToRomanLevels[firstHigherLevelDecimalIndex - 1] : _.last(decimalToRomanLevels);
+    if (_.isNil(lastDecimalLessThanValue)) {
+      throw new Error(`Can't convert '${complexDecimalToConvert}'`);
+    }
+
+    const romanSign = decimalToRomanSigns[`${lastDecimalLessThanValue}`];
+    const times = Math.floor(complexValueLeft / lastDecimalLessThanValue);
+    accComplexRoman += Array(times + 1).join(romanSign);
+    complexValueLeft -= times * lastDecimalLessThanValue;
+  }
+
+  const remainingDecimal = `${+decimalStr - complexValue}`;
+  return fromDecimalTail(remainingDecimal, `${accComplexRoman}${accRomanNumber}`);
+};
+
+export const fromDecimal = (decimal: number): string => {
+  return fromDecimalTail(`${decimal}`, ``);
+}
+
+export const fromRoman = (romanNumber: string): number => {
+  return fromRomanTail(romanNumber);
+};
